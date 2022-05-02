1bfa31f9975fef57db230526778c3c0ea077c00b
AshokKoda <a.koda59@gmail.com>
2022-05-02 13:19:49 +0530

    Added:Welcome message

---
+++ src/main/java/com/bridgelabz/indiancensus/IndianCensusAnalyzer.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.indiancensus;
+
+public class IndianCensusAnalyzer {
+
+	public static void main(String[] args) {
+		System.out.println("******************Indian Census Analyzer******************");
+
+	}
+
+}
